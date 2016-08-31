0b367c949ecbaa4ed9f22b80867ca89ae0988c97
edvardchen <edvardchen@tencent.com>
2016-08-31 12:13:57 +0800

    improve nyc test
    -- better ES6 supoort

--- src/index.spec.js
+++ src/index.spec.js
@@ -2,7 +2,7 @@
  * @Author: edvardchen
  * @Date:   2016-08-19 22:49:08
  * @Last Modified by:   edvardchen
- * @Last Modified time: 2016-08-19 22:52:47
+ * @Last Modified time: 2016-08-31 12:12:59
  */
 
 import 'should';
@@ -10,5 +10,8 @@ import 'should';
 import greeting from './';
 
 describe('greeting', () => {
-  it('type check', () => greeting.should.be.type('function'));
+  it('type check', () => {
+    greeting.should.be.type('function');
+    greeting();
+  });
 });
